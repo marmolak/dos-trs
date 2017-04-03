63b5adb8324f882362520346211b58fb834d6ed5
Robin Hack <hack.robin@gmail.com>
2017-04-03 21:23:05 +0200

    tsr: Show string.. :)

--- tsr.asm
+++ tsr.asm
@@ -5,13 +5,13 @@ org 100h
 
 ; resident part
     jmp setup ; such a waste of bytes!
-    mychar: db '1'
+    mystring: db 'Hello darkness my old friend...$'
 tsr:
     push ds
     push cs
     pop ds
-    mov ah, 02h
-    mov dx, mychar;
+    mov ah, 09h
+    mov dx, mystring
     int 21h
     mov ax, 4c00h
     int 21h
